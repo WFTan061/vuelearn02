38519c6e69425829d6576b222aa31619877a8998
Wua Fong Tan <wuafong92@live.com.sg>
2020-06-08 12:25:19 +0800

    First commit. layout complete for components version

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header testing = "1234"/>
+    <Table/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Table from './components/Table.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Table
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+	<div>
+		<b-button v-b-modal = "'modal-1'">Add</b-button>
+		
+		<b-modal id ="modal-1" title = "Add link">
+			<b-form @submit.prevent>
+				<b-form-group
+					id="name"
+					label = "Link Name"
+					label-for = "linkname"
+				>
+					<b-form-input
+					id="linkname"
+					v-model = "form.linkName"
+					type = "text"
+					required
+					placeholder = "Enter link name">
+					</b-form-input>
+				</b-form-group>
+				<b-form-group
+					id="address"
+					label = "Link address"
+					label-for = "linkaddress"
+				>
+					<b-form-input
+					id="linkaddress"
+					v-model = "form.linkAddress"
+					type = "text"
+					required
+					placeholder = "Enter link address">
+					</b-form-input>
+				</b-form-group>
+				<b-form-group
+					id="name"
+					label = "Link Icon"
+					label-for = "linkicon"
+				>
+					<b-form-input
+					id="linkicon"
+					v-model = "form.linkIcon"
+					type = "text"
+					required
+					placeholder = "Enter link icon path">
+					</b-form-input>
+				</b-form-group>
+			</b-form>
+		</b-modal>
+	</div>
+</template>
+<script>
+	export default{
+		props:['testing',],
+		data(){
+			return{
+				form:{
+					linkName:'',
+					linkAddress:'',
+					linkIcon:'',
+				}
+			}
+		}
+	}
+</script>
